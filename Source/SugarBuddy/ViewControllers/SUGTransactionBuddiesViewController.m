e420c92462e2c96eff215cadc1dbbe712d97423a
Daniele Sluijters <daenney@users.noreply.github.com>
2014-11-29 20:55:36 +0100

    Update the transaction view with data.

--- Source/SugarBuddy/ViewControllers/SUGTransactionBuddiesViewController.m
+++ Source/SugarBuddy/ViewControllers/SUGTransactionBuddiesViewController.m
@@ -17,6 +17,8 @@ static NSString * const SUGTransactionBuddiesCellID = @"cell-id";
 @interface SUGTransactionBuddiesViewController () <UICollectionViewDataSource, SUGBeaconReceiverDelegate>
 
 @property (nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic) IBOutlet UILabel *transactionTotal;
+@property (nonatomic) IBOutlet UILabel *totalPerPerson;
 
 @end
 
@@ -65,6 +67,10 @@ static NSString * const SUGTransactionBuddiesCellID = @"cell-id";
 - (void)reloadUIData
 {
     self.title = self.viewModel.transactionTitle;
+    self.transactionTotal.text = [self.viewModel transactionTitle];
+    self.totalPerPerson.text = [self.viewModel transactionSubtitle];
+
+    [[self collectionView] reloadData];
 }
 
 #pragma mark - UICollectionViewDelegate
