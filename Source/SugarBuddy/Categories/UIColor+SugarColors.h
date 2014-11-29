882612bcefa3711251b860b39f86845c8b3047f1
Hector Zarate <hecktorzr@gmail.com>
2014-11-29 15:17:57 +0100

    Created category on UIColor with colors used in the project

---
+++ Source/SugarBuddy/Categories/UIColor+SugarColors.h
@@ -0,0 +1,18 @@
+//
+//  UIColor+SugarColors.h
+//  SugarBuddy
+//
+//  Created by Hector Zarate on 11/29/14.
+//  Copyright (c) 2014 Hector Zarate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (SugarColors)
+
++ (UIColor *)SUGWhiteColor;
++ (UIColor *)SUGBlueBackgroundColor;
++ (UIColor *)SUGBlueHighlightColor;
++ (UIColor *)SUGNavyBlueBackgroundColor;
+
+@end
---
+++ Source/SugarBuddy/Categories/UIColor+SugarColors.m
@@ -0,0 +1,45 @@
+//
+//  UIColor+SugarColors.m
+//  SugarBuddy
+//
+//  Created by Hector Zarate on 11/29/14.
+//  Copyright (c) 2014 Hector Zarate. All rights reserved.
+//
+
+#import "UIColor+SugarColors.h"
+
+@implementation UIColor (SugarColors)
+
++ (UIColor *)SUGWhiteColor
+{
+    return [UIColor colorWithRed:1.0
+                           green:1.0
+                            blue:1.0
+                           alpha:1.0];
+}
++ (UIColor *)SUGBlueBackgroundColor
+{
+    return [UIColor colorWithRed:0.84
+                           green:0.88
+                            blue:0.90
+                           alpha:1.0];
+}
++ (UIColor *)SUGBlueHighlightColor
+{
+    return [UIColor colorWithRed:0.0
+                           green:0.64
+                            blue:0.93
+                           alpha:1.0];
+}
+
++ (UIColor *)SUGNavyBlueBackgroundColor
+{
+    return [UIColor colorWithRed:0.11
+                           green:0.15
+                            blue:0.19
+                           alpha:1.0];
+}
+
+
+
+@end
