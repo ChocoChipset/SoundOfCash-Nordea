9ceeb0b0b0d106491e2a9bb850b2ae382f2d8d45
Hector Zarate <hecktorzr@gmail.com>
2014-11-29 14:40:11 +0100

    Created class backend manager

---
+++ Source/SugarBuddy/SUGBackendManager.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface SUGBackendManager : NSObject
+
++ (instancetype)sharedManager;
+
+@end
---
+++ Source/SugarBuddy/SUGBackendManager.m
@@ -0,0 +1,24 @@
+//
+//  SUGBackendManager.m
+//  SugarBuddy
+//
+//  Created by Hector Zarate on 11/29/14.
+//  Copyright (c) 2014 Hector Zarate. All rights reserved.
+//
+
+#import "SUGBackendManager.h"
+
+static SUGBackendManager *static_backendManager = nil;
+
+@implementation SUGBackendManager
+
++ (instancetype)sharedManager
+{
+    if (!static_backendManager) {
+        static_backendManager = [[self alloc] init];
+    }
+    
+    return static_backendManager;
+}
+
+@end
